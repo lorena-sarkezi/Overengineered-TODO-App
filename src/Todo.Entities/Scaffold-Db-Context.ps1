5fcf4cc7f353d34fd3dcdcfc1d8a4a7799b69de7
Lorena Sarkezi <lorena.sarkezi@gmail.com>
2025-10-25 20:21:22 +0200

    Appsettings fallback for db context scaffold

--- src/Todo.Entities/Scaffold-Db-Context.ps1
+++ src/Todo.Entities/Scaffold-Db-Context.ps1
@@ -1,6 +1,30 @@
 param(
     [Parameter()]
-    [string]$connectionString
+    [string]$connectionString = $null
 )
 
-dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer --output-dir Models --context-dir ./ --force --no-onconfiguring
+function Get-ConnectionStringFromAppSettings {
+    $appSettingsPath = "..\Todo.Main\appsettings.json" 
+
+    $appSettingsJsonObject =  Get-Content -Path $appSettingsPath | ConvertFrom-Json
+    $connectionString = $appSettingsJsonObject.DatabaseOptions.ConnectionString
+    $connectionString
+}
+
+function Get-ConnectionString{
+
+    param(
+        [string]$connectionStringFromParam
+    )
+
+    if($connectionStringFromParam){
+        $connectionStringFromParam
+    }
+    else{
+        $appSettingsConnectionString = Get-ConnectionStringFromAppSettings
+        $appSettingsConnectionString
+    }
+}
+
+$actualConnectionString = Get-ConnectionString -connectionStringFromParam $connectionString
+dotnet ef dbcontext scaffold $actualConnectionString Microsoft.EntityFrameworkCore.SqlServer --output-dir Models --context-dir ./ --force --no-onconfiguring
