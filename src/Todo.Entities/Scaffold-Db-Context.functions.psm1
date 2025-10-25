8d1da9834b62357b786a848405cff12965d62f8d
Lorena Sarkezi <lorena.sarkezi@gmail.com>
2025-10-25 20:26:20 +0200

    Move ps functions to separate module

---
+++ src/Todo.Entities/Scaffold-Db-Context.functions.psm1
@@ -0,0 +1,23 @@
+$appSettingsPath = "..\Todo.Main\appsettings.json" 
+
+function Get-ConnectionStringFromAppSettings {
+    $appSettingsJsonObject =  Get-Content -Path $appSettingsPath | ConvertFrom-Json
+    $connectionString = $appSettingsJsonObject.DatabaseOptions.ConnectionString
+    $connectionString
+}
+
+function Get-ConnectionString{
+    param(
+        [string]$connectionStringFromParam
+    )
+
+    if($connectionStringFromParam){
+        $connectionStringFromParam
+    }
+    else{
+        $appSettingsConnectionString = Get-ConnectionStringFromAppSettings
+        $appSettingsConnectionString
+    }
+}
+
+Export-ModuleMember -Function Get-ConnectionString
--- src/Todo.Entities/Scaffold-Db-Context.ps1
+++ src/Todo.Entities/Scaffold-Db-Context.ps1
@@ -3,28 +3,7 @@ param(
     [string]$connectionString = $null
 )
 
-function Get-ConnectionStringFromAppSettings {
-    $appSettingsPath = "..\Todo.Main\appsettings.json" 
-
-    $appSettingsJsonObject =  Get-Content -Path $appSettingsPath | ConvertFrom-Json
-    $connectionString = $appSettingsJsonObject.DatabaseOptions.ConnectionString
-    $connectionString
-}
-
-function Get-ConnectionString{
-
-    param(
-        [string]$connectionStringFromParam
-    )
-
-    if($connectionStringFromParam){
-        $connectionStringFromParam
-    }
-    else{
-        $appSettingsConnectionString = Get-ConnectionStringFromAppSettings
-        $appSettingsConnectionString
-    }
-}
+Import-Module ./Scaffold-Db-Context.functions.psm1
 
 $actualConnectionString = Get-ConnectionString -connectionStringFromParam $connectionString
 dotnet ef dbcontext scaffold $actualConnectionString Microsoft.EntityFrameworkCore.SqlServer --output-dir Models --context-dir ./ --force --no-onconfiguring
